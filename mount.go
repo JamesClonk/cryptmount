9c04f2f2be04e059a793dc5c8c5c6ca65f73589a
JamesClonk <jamesclonk@jamesclonk.ch>
2015-01-04 15:15:44 +0100

    updated

--- mount.go
+++ mount.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
+	"log"
+	"os"
+	"os/exec"
 	"strings"
+	"time"
 )
 
 var (
@@ -20,8 +24,33 @@ func mountVolume(device string, mountpoint string) {
 }
 
 func luksOpen(device string, name string) {
-	out := system(fmt.Sprintf(LUKSOPEN_CMD, device, name))
-	fmt.Println(out)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("\nEnter passphrase for %v:", device)
+	passphrase, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	passphrase = strings.Trim(passphrase, "\t\n\r ")
+	if passphrase == "" {
+		log.Fatalf("%v", boldRedBlinking("No passphrase given!"))
+	}
+
+	cmds := strings.Split(fmt.Sprintf(LUKSOPEN_CMD, device, name), " ")
+	cmd := exec.Command(cmds[0], cmds[1:]...)
+	//cmd := exec.Command("cryptsetup", "luksOpen", "/dev/sdc2", "sdc2")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	stdin, _ := cmd.StdinPipe()
+	stdin.Write([]byte(passphrase + "\n"))
+
+	time.AfterFunc(time.Duration(2)*time.Second, func() {
+		if !cmd.ProcessState.Exited() {
+			log.Fatalf("\n%v\n", boldRedBlinking("Incorrect passphrase!"))
+		}
+	})
+
+	cmd.Run()
 }
 
 func mount(name string, mountpoint string) {
